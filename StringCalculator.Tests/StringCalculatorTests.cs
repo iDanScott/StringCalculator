a840bbc993f433b34a86d5f9374dc40097d7388b
Dan Scott <tehdanscott@gmail.com>
2018-06-22 23:08:39 +0100

    Initial Commit

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Add_1_Returns1()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("1");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Add_12_Returns3()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("1,2");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_12Newline_Returns3()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("1\n2");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_1To10_Returns55()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("1,2,3,4,5,6,7,8,9,10");
+            Assert.Equal(55, result);
+        }
+
+        [Fact]
+        public void Add_Nothing_Returns0()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Add_12NewDelimiter_Returns3()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("//;\n1;2");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_Negative1Negative2_ThrowsException()
+        {
+            StringCalculator calc = GetNewCalculator();
+            FormatException fEx = Assert.Throws<FormatException>(() => calc.Add("-1,-2"));
+            Assert.Equal("negatives not allowed: -1, -2", fEx.Message);
+        }
+
+        [Fact]
+        public void Add_2_1001_Returns2_1001Ignored()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("2,1001");
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Add_AnyLengthDelimiter_123_Returns6()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("//***\n1***2***3");
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Add_123_MultipleDelimiter_Returns6()
+        {
+            StringCalculator calc = GetNewCalculator();
+            int result = calc.Add("//[*][%]\n1*2%3");
+            Assert.Equal(6, result);
+        }
+
+        public StringCalculator GetNewCalculator()
+        {
+            StringCalculator calculator = new StringCalculator();
+            calculator.Initialise();
+            return calculator;
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        List<string> Delimiters;
+        public void Initialise()
+        {
+            Delimiters = new List<string>() { ",", "\n" };
+        }
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            string[] delimited = numbers.Split(Delimiters.ToArray(), StringSplitOptions.None);
+            if (delimited.Length > 0)
+            {
+                if (delimited[0].Length > 2)
+                {
+                    if (delimited[0].Substring(0, 2) == "//")
+                    {
+                        Delimiters.Remove(",");
+                        string delimiterString = delimited[0].Substring(2, delimited[0].Length - 2);
+                        if (delimiterString.Contains("["))
+                        {
+                            string[] regexMatch = Regex.Split(delimiterString, "\\[(.*?)\\]");
+                            foreach (string match in regexMatch)
+                            {
+                                if (!string.IsNullOrEmpty(match))
+                                {
+                                    Delimiters.Add(match);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Delimiters.Add(delimiterString);
+                        }
+                        delimited = delimited.Where(x => x.Substring(0, 2) != "//").ToArray();
+                    }
+                }
+
+                delimited = string.Join(Delimiters[0], delimited).Split(Delimiters.ToArray(), StringSplitOptions.None);
+                List<int> negatives = new List<int>();
+                int sum = 0;
+                foreach (string number in delimited)
+                {
+                    int parsed = Int32.Parse(number);
+                    if (parsed > 0)
+                    {
+                        if (parsed <= 1000)
+                        {
+                            sum += parsed;
+                        }
+                    }
+                    else
+                    {
+                        negatives.Add(parsed);
+                    }
+                }
+                if (negatives.Count > 0)
+                {
+                    throw new FormatException("negatives not allowed: " + string.Join(", ", negatives.ToArray()));
+                }
+                return sum;
+            }
+            throw new Exception();
+        }
+    }
+}
